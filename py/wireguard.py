726ed0486e1e7a693ffabb22eb88ef6bc8c69277
Jarno <jarnoaxelbakker@hotmail.com>
2022-01-21 12:14:15 +0100

    allow enabling vpn from python

--- py/wireguard.py
+++ py/wireguard.py
@@ -1,6 +1,10 @@
 from ctypes import *
+import re
 
 c_library = CDLL('../c/library.so')
+
+def valid_interface(interface):
+    return bool(re.match("^[A-Za-z0-9_-]*$", interface))
 
 def empty_key():
     return create_string_buffer(b'\000' * 32)
@@ -11,8 +15,12 @@ def key_pair():
     c_library.generate_public_key(private, public)
     return private, public
 
-def create_device(name, port, private_key):
-    c_library.add_device(name.encode(), c_ushort(port), private_key)
+def create_device(name, port, private_key, local_ip):
+    if (valid_interface(name)):
+        c_library.add_device(name.encode(), c_ushort(port), private_key)
+        exec("ip a add dev {} {}".format(name, local_ip))
+    else:
+        print("invalid device name '{}'".format(device))
 
 def add_peer(device_name, public_key, address, port):
     c_library.add_server_peer(device_name.encode(), public_key, address.encode(), c_ushort(port))
@@ -38,3 +46,9 @@ def key_from_base64(base64):
     key = empty_key()
     c_library.key_from_string(base64.encode(), key)
     return key
+
+def enable_device(device):
+    if (valid_interface(device)):
+        exec("ip link set up {}".format(device))
+    else:
+        print("invalid device '{}'".format(device))
