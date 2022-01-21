ea64fa20f2907ce9eeaeb9dcccd9a554d55388da
Jarno <jarnoaxelbakker@hotmail.com>
2022-01-21 14:47:39 +0100

    added .env

--- py/test_client.py
+++ py/test_client.py
@@ -2,24 +2,28 @@
 
 import requests
 import wireguard as wg
+import sys
 
 IP_ADDR = "209.97.176.198"
 WG_PORT = 1234
 HTTP_PORT = 8888
-HTTP_FULL = "http://{}:{}".format(IP_ADDR, HTTP_PORT)
+HTTP_FORMAT = "http://{}:{}"
 
-private, public = wg.key_pair()
-public_string = wg.key_to_base64(public)
+if __name__ == '__main__':
 
-data = {
-    'key': public_string
-}
+    private, public = wg.key_pair()
+    public_string = wg.key_to_base64(public)
 
-response = requests.post(url=HTTP_FULL, json=data).json()
+    data = {
+        'key': public_string
+    }
 
-srv_key = wg.key_from_base64(response['public_key'])
-local_ip = response['ip']
+    endpoint = HTTP_FORMAT.format(IP_ADDR, HTTP_PORT)
+    response = requests.post(url=HTTP_FULL, json=data).json()
 
-wg.delete_device('wg-client')
-wg.setup_client_connection('wg-client', private, "{}/24".format(local_ip), srv_key, IP_ADDR, WG_PORT)
-wg.enable_device('wg-client')
+    srv_key = wg.key_from_base64(response['public_key'])
+    local_ip = response['ip']
+
+    wg.delete_device('wg-client')
+    wg.setup_client_connection('wg-client', private, "{}/24".format(local_ip), srv_key, IP_ADDR, WG_PORT)
+    wg.enable_device('wg-client')
