ecb27c78689de503c622920a1083bbae6a64eb44
Jarno <jarnoaxelbakker@hotmail.com>
2022-02-04 13:45:18 +0100

    pylint commpliance

--- python_wireguard/client.py
+++ python_wireguard/client.py
@@ -1,11 +1,17 @@
+'''
+Contains a class representing a client-side Wireguard connection.
+'''
 import python_wireguard.wireguard as wg
 from .key import Key
 from .server_connection import ServerConnection
 
 class Client:
+    '''
+    This is a client-side Wireguard connection.
+    '''
     def __init__(self, interface_name, key, local_ip):
         if not wg.valid_interface(interface_name):
-            raise ValueError("Invalid interface name {}".format(interface_name))
+            raise ValueError(f"Invalid interface name {interface_name}")
         if not isinstance(key, Key):
             raise ValueError("Key should be an instance of python_wireguard.Key")
 
@@ -14,28 +20,44 @@ class Client:
         self.interface_name = interface_name
         self.connection = None
         self.interface_created = False
-    
+
     def create_interface(self):
+        '''
+        Create the internet interface belonging to this client.
+        '''
         wg.create_client(self.interface_name, self.key.as_bytes(), self.local_ip)
         self.interface_created = True
-    
+
     def delete_interface(self):
+        '''
+        Delete the internet interface belonging to this client.
+        '''
         wg.delete_device(self.interface_name)
         self.interface_created = False
-    
+
     def set_server(self, server_connection):
         '''
         Set the server for this Client.
         '''
         if not isinstance(server_connection, ServerConnection):
-            raise ValueError("server_connection should be an instance of python_wireguard.ServerConnection")
+            raise ValueError(
+                "server_connection should be an instance of python_wireguard.ServerConnection"
+                )
+        self.connection = server_connection
 
     def connect(self):
+        '''
+        Connect to the Wireguard server.
+        '''
         if not self.interface_created:
             self.create_interface()
-        if self.connection == None:
-            raise ValueError("The connection has not been configured for this Client yet. Use 'set_server' to set it.")
+        if self.connection is None:
+            raise ValueError(
+                "The connection has not been configured for this Client yet. \
+                Use 'set_server' to set it."
+                )
         server_connection = self.connection
         key = server_connection.get_key()
-        wg.client_add_peer(self.interface_name, key.as_bytes(), server_connection.get_endpoint(), server_connection.get_port())
+        wg.client_add_peer(self.interface_name, key.as_bytes(),
+            server_connection.get_endpoint(), server_connection.get_port())
         wg.enable_device(self.interface_name)
--- python_wireguard/key.py
+++ python_wireguard/key.py
@@ -8,25 +8,37 @@ class Key:
     This represents a Wireguard key.
     '''
     def __init__(self, base64_string=None):
-        if base64_string == None:
+        if base64_string is None:
             self.byte_content = wg.empty_key()
         elif len(base64_string) != 44:
             raise ValueError("Invalid base64 string provided")
         else:
             self.byte_content = wg.key_from_base64(base64_string)
-    
+
     def __str__(self):
+        '''
+        Convert this key to a string.
+        '''
         return wg.key_to_base64(self.byte_content)
-    
+
     def __repr__(self):
-        return "<python_wireguard.Key object: {}>".format(str(self))
-    
+        '''
+        Represent this class (used in the python cli).
+        '''
+        return f"<python_wireguard.Key object: {str(self)}>"
+
     @staticmethod
     def key_pair():
+        '''
+        Create a new private/public key pair.
+        '''
         private, public = wg.key_pair()
         private_obj = Key(wg.key_to_base64(private))
         public_obj = Key(wg.key_to_base64(public))
         return private_obj, public_obj
-        
+
     def as_bytes(self):
+        '''
+        Get the c_char_array content of this key.
+        '''
         return self.byte_content
