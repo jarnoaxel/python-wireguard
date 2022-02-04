6b0cf5e7e6628340ebd7bd66fe6d36e4d8a789dc
Jarno <jarnoaxelbakker@hotmail.com>
2022-02-04 14:34:49 +0100

    removed helper functions from module init

--- python_wireguard/__init__.py
+++ python_wireguard/__init__.py
@@ -1,9 +1,7 @@
 '''
 This module is a wrapper for interacting with Wireguard on Linux.
 '''
-from .wireguard import valid_interface, empty_key, key_pair, create_server, create_client, \
-    client_add_peer, setup_client_connection, server_add_peer, delete_device, list_devices, \
-    print_key, key_to_base64, key_from_base64, enable_device
+from .wireguard import list_devices, delete_device
 from .key import Key
 from .client import Client
 from .server_connection import ServerConnection
